df1217237538e5fb412a26101ad6e83bfe2bf0d2
h-sugawara <nnfxt540@gmail.com>
2014-02-24 17:52:29 +0900

    added core class unittest files

---
+++ src/tests/phpingguo/core/ServerTest.php
@@ -0,0 +1,28 @@
+<?php
+use Phpingguo\System\Core\Server;
+
+class ServerTest extends PHPUnit_Framework_TestCase
+{
+    public function provider()
+    {
+        return [
+            [ [ Server::SRV_ENV_NAME => 'local' ], [ Server::SRV_ENV_NAME => 'SRV_ENV_NAME' ], true ],
+            [ [ Server::SRV_ENV_NAME => null ], [ Server::SRV_ENV_NAME => 'SRV_ENV_NAME_2' ], false ],
+        ];
+    }
+    
+    /**
+     * @dataProvider provider
+     */
+    public function test($server_vals, $call_statics, $registry)
+    {
+        ($registry === true) && $_SERVER = $server_vals;
+        
+        Server::capture();
+        
+        foreach ($server_vals as $key => $value) {
+            $this->assertSame($value, Server::getValue($key));
+            $this->assertSame($value, Server::{$call_statics[$key]}($key));
+        }
+    }
+}
