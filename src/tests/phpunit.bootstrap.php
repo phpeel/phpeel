5deafb1f4c09a1547fa1eda1b4ac13b3ab8fc09b
h-sugawara <nnfxt540@gmail.com>
2014-02-24 17:51:40 +0900

    added phpunit unittest init files

---
+++ src/tests/phpunit.bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+date_default_timezone_set('Asia/Tokyo');
+
+defined('AIGIS_BOOT_TYPE') || define('AIGIS_BOOT_TYPE', 'public');
+defined('AIGIS_START_TIME') || define('AIGIS_START_TIME', microtime(true));
+defined('AIGIS_START_MEMORY') || define('AIGIS_START_MEMORY', memory_get_usage());
