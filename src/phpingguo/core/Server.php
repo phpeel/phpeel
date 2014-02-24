a0bc59c448c78d62875b16b8a6db39aa18c70b46
h-sugawara <nnfxt540@gmail.com>
2014-02-24 14:55:51 +0900

    added core class files

---
+++ src/phpingguo/core/Server.php
@@ -0,0 +1,82 @@
+<?php
+namespace Phpingguo\System\Core;
+
+use Phpingguo\System\Exts\Lib\Common\Arrays;
+
+/**
+ * サーバー情報を保持するクラスです。
+ * 
+ * @final [継承禁止クラス]
+ * @author hiroki sugawara
+ */
+final class Server
+{
+    // ---------------------------------------------------------------------------------------------
+    // const
+    // ---------------------------------------------------------------------------------------------
+    const SRV_ENV_NAME    = 'SERVER_ENVIRONMENT_NAME';
+    const REQUEST_METHOD  = 'REQUEST_METHOD';
+    const REMOTE_ADDR     = 'REMOTE_ADDR';
+    const HTTP_HOST       = 'HTTP_HOST';
+    const PATH_INFO       = 'PATH_INFO';
+    const REQUEST_URI     = 'REQUEST_URI';
+    const HTTP_REFERER    = 'HTTP_REFERER';
+    const HTTP_USER_AGENT = 'HTTP_USER_AGENT';
+    const AUTH_TYPE       = 'AUTH_TYPE';
+    const PHP_AUTH_DIGEST = 'PHP_AUTH_DIGEST';
+    const PHP_AUTH_USER   = 'PHP_AUTH_USER';
+    const PHP_AUTH_PW     = 'PHP_AUTH_PW';
+    
+    // ---------------------------------------------------------------------------------------------
+    // class fields
+    // ---------------------------------------------------------------------------------------------
+    private static $server_vals = [];
+    
+    // ---------------------------------------------------------------------------------------------
+    // public class methods
+    // ---------------------------------------------------------------------------------------------
+    /**
+     * サーバー情報をキャプチャーします。
+     * 
+     * @return Boolean サーバー情報をキャプチャできた場合は true を、それ以外の場合は false を返します。
+     */
+    public static function capture()
+    {
+        $result = Arrays::copyWhen(empty($_SERVER) === false, static::$server_vals, $_SERVER);
+        Arrays::clear($_SERVER);
+        
+        return $result;
+    }
+    
+    /**
+     * サーバー情報から指定した名前に該当する値を取得します。
+     * 
+     * @param String $name	取得する情報の名前
+     * @param Array $args	値が存在しなかった場合のデフォルト値
+     * 
+     * @return String サーバー情報から指定した名前に該当する値を返します。
+     */
+    public static function __callStatic($name, $args)
+    {
+        $class = get_called_class();
+        
+        if (false === ($const = (new \ReflectionClass($class))->getConstant($name))) {
+            return null;
+        }
+        
+        return $class::getValue($const, Arrays::isValid($args) ? $args[0] : null);
+    }
+    
+    /**
+     * サーバー情報から指定した名前に該当する値を取得します。
+     * 
+     * @param String $name					取得する情報の名前
+     * @param mixed $default [初期値=null]	値が存在しなかった場合のデフォルト値
+     * 
+     * @return String サーバー情報から指定した名前に該当する値を返します。
+     */
+    public static function getValue($name, $default = null)
+    {
+        return Arrays::getValue(static::$server_vals, $name, $default);
+    }
+}
