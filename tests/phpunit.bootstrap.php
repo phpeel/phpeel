591dbb78f522ce676fd31da5f08515f89e39c000
h-sugawara <nnfxt540@gmail.com>
2014-03-10 10:16:01 +0900

    moved phpunit.bootstrap.php

--- tests/phpunit.bootstrap.php
+++ tests/phpunit.bootstrap.php
@@ -6,4 +6,4 @@ defined('AIGIS_BOOT_TYPE') || define('AIGIS_BOOT_TYPE', 'public');
 defined('AIGIS_START_TIME') || define('AIGIS_START_TIME', microtime(true));
 defined('AIGIS_START_MEMORY') || define('AIGIS_START_MEMORY', memory_get_usage());
 
-require realpath(__DIR__) . '/../../vendor/autoload.php';
+require realpath(__DIR__) . '/../vendor/autoload.php';
