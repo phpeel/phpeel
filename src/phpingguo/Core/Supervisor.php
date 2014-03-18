1f517b9ae6322848a93a4f236532359e6b412eef
h-sugawara <nnfxt540@gmail.com>
2014-03-18 13:20:27 +0900

    changed Supervisor class file

--- src/phpingguo/Core/Supervisor.php
+++ src/phpingguo/Core/Supervisor.php
@@ -17,6 +17,10 @@ final class Supervisor
     // ---------------------------------------------------------------------------------------------
     const PATH_CONFIG_DI         = 'di_preset_services';
     const PATH_CONFIG_REGULATION = 'various_regulations';
+    const ENUM_CONTENT_TYPE      = 'ContentType';
+    const ENUM_HTTP_METHOD       = 'HttpMethod';
+    const ENUM_RESPONSE_CODE     = 'ResponseCode';
+    const ENUM_X_FRAME_OPTIONS   = 'XFrameOptions';
 
     // ---------------------------------------------------------------------------------------------
     // public static methods
@@ -94,5 +98,18 @@ final class Supervisor
             CString::unionDirectoryPath(static::getAppPath(), 'cache'),
             $sub_dir_name
         );
+    }
+
+    /**
+     * 列挙型クラスの名前空間付きの完全修飾名を取得します。
+     * 
+     * @param String $enum_name 完全修飾名を取得する列挙型クラスの名前
+     * 
+     * @throws \InvalidArgumentException 有効な列挙型クラスではなかった場合
+     * @return String 列挙型クラスの名前空間付きの完全修飾名
+     */
+    public static function getEnumFullName($enum_name)
+    {
+        return CString::getEnumFullName("Phpingguo\\System\\Enums\\", $enum_name);
     }
 }
