1649afa600953c0b49199c48884c1ef7e65b8cd6
h-sugawara <nnfxt540@gmail.com>
2014-04-06 11:05:42 +0900

    changed Supervisor class unittest file

--- tests/phpingguo/Core/SupervisorTest.php
+++ tests/phpingguo/Core/SupervisorTest.php
@@ -12,11 +12,15 @@ class SupervisorTest extends \PHPUnit_Framework_TestCase
         );
         $project_path = $root_path . DIRECTORY_SEPARATOR . 'src';
         $app_path     = $project_path . DIRECTORY_SEPARATOR . 'app';
+        $config_path  = $project_path . DIRECTORY_SEPARATOR . 'config';
+        $srv_env_path = $config_path . DIRECTORY_SEPARATOR . 'server_environments';
         
         $this->assertSame($project_path, Supervisor::getProjectPath());
         $this->assertSame($project_path . DIRECTORY_SEPARATOR . 'phpingguo', Supervisor::getSystemPath());
-        $this->assertSame($project_path . DIRECTORY_SEPARATOR . 'config', Supervisor::getConfigPath());
+        $this->assertSame($config_path, Supervisor::getConfigPath());
+        $this->assertSame($srv_env_path . DIRECTORY_SEPARATOR . 'local', Supervisor::getServerEnvPath());
         $this->assertSame($app_path, Supervisor::getAppPath());
+        $this->assertSame($app_path . DIRECTORY_SEPARATOR . 'View', Supervisor::getViewPath());
         $this->assertSame($app_path . DIRECTORY_SEPARATOR . 'cache', Supervisor::getCachePath());
     }
 }
