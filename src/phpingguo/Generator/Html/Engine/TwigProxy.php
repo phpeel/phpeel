6590cf83b8129c86d8078e425ea684a1cddea8af
h-sugawara <nnfxt540@gmail.com>
2014-05-21 22:57:59 +0900

    added TwigAdapter class and changed TwigProxy class

--- src/phpingguo/Generator/Html/Engine/TwigProxy.php
+++ src/phpingguo/Generator/Html/Engine/TwigProxy.php
@@ -1,7 +1,10 @@
 <?php
 namespace Phpingguo\System\Generator\Html\Engine;
 
-use Phpingguo\System\Generator\Html\IHtmlGenerator;
+use Phpingguo\ApricotLib\Common\Arrays;
+use Phpingguo\ApricotLib\Enums\Charset;
+use Phpingguo\System\Core\Supervisor;
+use Phpingguo\System\Generator\Html\BaseEngineProxy;
 use Phpingguo\System\Module\BaseModule;
 
 /**
@@ -10,13 +13,40 @@ use Phpingguo\System\Module\BaseModule;
  * @final [継承禁止クラス]
  * @author hiroki sugawara
  */
-final class TwigProxy implements IHtmlGenerator
+final class TwigProxy extends BaseEngineProxy
 {
+    // ---------------------------------------------------------------------------------------------
+    // public member methods
+    // ---------------------------------------------------------------------------------------------
     /**
      * @see IHtmlGenerator::render
      */
     public function render(BaseModule $module, array $options)
     {
-        return  __CLASS__;
+        $this->isInitialized() || $this->initEngineInstance($options);
+        
+        return $this->getEngineInstance()->rendering($module);
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // private member methods
+    // ---------------------------------------------------------------------------------------------
+    /**
+     * Twigのテンプレートエンジンを操作するクラスのインスタンスを初期化します。
+     * 
+     * @param Array $options 初期化オプション
+     */
+    private function initEngineInstance(array $options)
+    {
+        $twig_loader  = new \Twig_Loader_Filesystem([ Supervisor::getViewPath() ]);
+        $twig_options = [
+            'debug'       => Arrays::getValue($options, 'DebugMode', true),
+            'charset'     => Arrays::getValue($options, 'Charset', Charset::UTF8),
+            'cache'       => Supervisor::getCachePath(Supervisor::PATH_CACHE_TWIG),
+            'auto_reload' => Arrays::getValue($options, 'Recompile', true),
+            'autoescape'  => Arrays::getValue($options, 'AutoEscape', true),
+        ];
+        
+        $this->setEngineInstance($this->createInstance('TwigAdapter', [ $twig_loader, $twig_options ]));
     }
 }
