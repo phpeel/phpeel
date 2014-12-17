eff4f087a5ab67a28d3861d430c1fc6fcff194e0
h-sugawara <nnfxt540@gmail.com>
2014-12-17 16:12:16 +0900

    Changed FIlterHost class.

--- src/phpeel/Filter/Post/FilterHost.php
+++ src/phpeel/Filter/Post/FilterHost.php
@@ -3,10 +3,11 @@ namespace Phpeel\System\Filter\Post;
 
 use Phpeel\System\Core\Supervisor;
 use Phpeel\System\Filter\BaseFilterHost;
+use Phpeel\System\Module\BaseModule;
 
 /**
  * 事後型フィルタを管理するクラスです。
- * 
+ *
  * @final [継承禁止クラス]
  * @author hiroki sugawara
  */
@@ -17,26 +18,26 @@ final class FilterHost extends BaseFilterHost
     // ---------------------------------------------------------------------------------------------
     /**
      * FilterHost クラスのインスタンスを取得します。
-     * 
+     *
      * @return FilterHost 初回呼び出し時は新しいインスタンス。それ以降の時は生成済みのインスタンス。
      */
     public static function getInstance()
     {
         return Supervisor::getDiContainer(Supervisor::DIS_SYSTEM)->get(__CLASS__);
     }
-    
+
     // ---------------------------------------------------------------------------------------------
     // public member methods
     // ---------------------------------------------------------------------------------------------
     /**
      * 登録したフィルタオブジェクトの処理を全て適用します。
-     * 
-     * @param AppModule $module フィルタオブジェクトを適用するモジュールクラスのインスタンス
-     * 
-     * @return AppModule フィルタオブジェクトを適用したモジュールクラスのインスタンス
+     *
+     * @param BaseModule $module フィルタオブジェクトを適用するモジュールクラスのインスタンス
+     *
+     * @return BaseModule フィルタオブジェクトを適用したモジュールクラスのインスタンス
      */
-    public function apply(AppModule $module)
+    public function apply(BaseModule $module)
     {
-        return $this->applyFilters(__NAMESPACE__, $module);
+        return $this->applyFilters('Post', __NAMESPACE__, $module);
     }
 }
--- src/phpeel/Filter/Pre/FilterHost.php
+++ src/phpeel/Filter/Pre/FilterHost.php
@@ -2,11 +2,12 @@
 namespace Phpeel\System\Filter\Pre;
 
 use Phpeel\System\Core\Supervisor;
+use Phpeel\System\Enums\FilterType;
 use Phpeel\System\Filter\BaseFilterHost;
 
 /**
  * 事前型フィルタを管理するクラスです。
- * 
+ *
  * @final [継承禁止クラス]
  * @author hiroki sugawara
  */
@@ -17,14 +18,14 @@ final class FilterHost extends BaseFilterHost
     // ---------------------------------------------------------------------------------------------
     /**
      * FilterHost クラスのインスタンスを取得します。
-     * 
+     *
      * @return FilterHost 初回呼び出し時は新しいインスタンス。それ以降の時は生成済みのインスタンス。
      */
     public static function getInstance()
     {
         return Supervisor::getDiContainer(Supervisor::DIS_SYSTEM)->get(__CLASS__);
     }
-    
+
     // ---------------------------------------------------------------------------------------------
     // public member methods
     // ---------------------------------------------------------------------------------------------
@@ -33,6 +34,6 @@ final class FilterHost extends BaseFilterHost
      */
     public function apply()
     {
-        $this->applyFilters(__NAMESPACE__);
+        $this->applyFilters(FilterType::PRE, __NAMESPACE__);
     }
 }
